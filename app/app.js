5196aba03d3ad70b06e83c513f7de5d267bb560d
Chris Stephens <chrisjstephens@yahoo.com>
2015-04-08 22:18:24 -0400

    April 8th Update
    -Movies are loaded via $http call rather then hardcoded into controller,
    two different json files which are loaded depending on current day
    -Updated form validation in several areas to prevent invalid submissions

--- app/app.js
+++ app/app.js
@@ -21,6 +21,17 @@ app.config(function($routeProvider) {
 	});
 });
 
+app.factory('CurrentMovies', function(){
+	
+	var currMov = {
+		aa : 'workplz',
+		obj : { }
+	};
+	
+	return currMov;
+		
+});
+
 app.factory('FormValues', function(){
 	//Custom factory that allows values to be passed in between different pages in the ng-view form.	
 	var formVals = {
@@ -28,15 +39,20 @@ app.factory('FormValues', function(){
 		custPhone : '',
 		movieChosen : '',
 		movieTime : '',
-		movieTickets : '',
+		movieTickets : 0,
 		movieCost : 0,
 		concObjects : {},
 		concPurchases : '',
 		concTotalCost : 0,
+		concItems: false,
 		finalCost : 0,
 		tax : 0,
-		subTotal : 0
+		subTotal : 0,
+		form1: false,
+		form2: false,
+		finalForm: false
 	};
 	
 	return formVals;
 });
+
--- app/controllers/moviezController.js
+++ app/controllers/moviezController.js
@@ -1,149 +1,61 @@
-app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
+app.controller('MoviezCtrl', function($http, $scope, $location, CurrentMovies, FormValues) {
 	
-	/*$http.get('app/data/movies.json')
-	.success(function (response) {
-		this.MoviesInfo = response;
-	});*/ 
+	//Give custom factory to all form values so they can change pages
+	$scope.fV = FormValues;
+	$scope.cM = CurrentMovies;
 	
-	$scope.MoviesInfo = [
-	{	
-		id: 1,
-		moviename : "Battlefield Earth",
-		releasedate : "12 May 2000",
-		imdbLink : "http://www.imdb.com/title/tt0185183",
-		price : "500",
-		times: [
-			"1:00 PM",
-			"1:30 PM",
-			"2:00 PM",
-			"4:00 PM",
-			"5:00 PM",
-			"6:00 PM",
-			"7:00 PM",
-			"8:00 PM",
-			"9:00 PM",
-			"10:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 2,
-		moviename : "Ace Ventura: The Pet Detective",
-		releasedate : "4 February 1994",
-		imdbLink : "http://www.imdb.com/title/tt0109040",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"2:00 PM",
-			"4:00 PM",
-			"5:00 PM",
-			"6:00 PM",
-			"7:00 PM",
-			"8:00 PM",
-			"9:00 PM",
-			"10:00 PM"
-		]
-	},
-	{
-		id: 3,
-		moviename : "Armageddon",
-		releasedate : "1 July 1998",
-		imdbLink : "http://www.imdb.com/title/tt0120591",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 4,
-		moviename : "Deep Impact",
-		releasedate : "8 May 1998",
-		imdbLink : "http://www.imdb.com/title/tt0120647",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 5,
-		moviename : "Mars Attacks!",
-		releasedate : "13 December 1996",
-		imdbLink : "http://www.imdb.com/title/tt0116996",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 6,
-		moviename : "The Lion King",
-		releasedate : "24 June 1994",
-		imdbLink : "http://www.imdb.com/title/tt0110357",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 7,
-		moviename : "Home Alone",
-		releasedate : "16 November 1990",
-		imdbLink : "http://www.imdb.com/title/tt0099785",
-		price : "10",
-		times: [
-			"1:00 PM",
-			"1:30 PM",
-			"2:00 PM",
-			"4:00 PM",
-			"5:00 PM",
-			"6:00 PM",
-			"7:00 PM",
-			"8:00 PM",
-			"9:00 PM",
-			"10:00 PM",
-			"11:00 PM",
-			"12:00 PM"
-		]
-	},
-	{
-		id: 8,
-		moviename : "Free Willy",
-		releasedate : "16 July 1993",
-		imdbLink : "http://www.imdb.com/title/tt0106965",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 9,
-		moviename : "Homeward Bound",
-		releasedate : "12 February 1993",
-		imdbLink : "http://www.imdb.com/title/tt0107131",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
-	},
-	{
-		id: 10,
-		moviename : "Space Jam",
-		releasedate : "15 November 1996",
-		imdbLink : "http://www.imdb.com/title/tt0117705",
-		price : "1",
-		times: [
-			"1:00 PM",
-			"11:00 PM"
-		]
+	$scope.currentDay = function () {
+		//Get current day to populate movie schedule
+		var d = new Date();
+		
+		var month = d.getMonth();
+		var day = d.getDate();
+		var year = "2022";
+		
+		var fullDate = month + " - " + day + " - " + year;
+		
+		var day = d.getDate();
+		if (day % 2 ){
+			//If the day is odd populate odd/00s movie schedule
+			console.log("Odd Day " + d.getDate());
+			$scope.movies00();
+			$scope.movieDate = fullDate
+			$scope.movieDay = "2000's";
+			$scope.movieDayType = "odd"
+		} else {
+			//If the day is odd populate even/90's movie schedule
+			console.log("Even Day " + d.getDate());
+			$scope.movies90();
+			$scope.movieDate = fullDate
+			$scope.movieDay = "1990's";
+			$scope.movieDayType = "even"
+		}
 	}
-	];
 	
+	//Get 2000's movies json file and populate cM.obj with movies
+	$scope.movies00 = function () {
+		$http.get('app/data/movies00.json')
+		.success(function (response) {
+			var movieData = response;
+			
+			$scope.cM.obj = movieData;
+			console.log("2000's movies loaded.");
+		});
+	}
+	
+	$scope.movies90 = function () {
+		$http.get('app/data/movies90.json')
+		.success(function (response) {
+			var movieData = response;
+			
+			$scope.cM.obj = movieData;
+			console.log($scope.cM.obj.length + " length");
+			
+			console.log("1990s movies loaded.");
+		});
+	}
+	
+	//JSON layout of concession items
 	$scope.concessions = 
 	[{	
 		id: 1,
@@ -176,17 +88,18 @@ app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
 	}];
 	
 	$scope.goToForm2 = function() {
+		$scope.fV.form1 = true;
 		$location.path('/movietime.html');
 	}
 	$scope.goToForm3 = function() {
+		$scope.fV.form2 = true;
 		$location.path('/concessions.html');
 	}
 	$scope.goToForm4 = function() {
 		$location.path('/confirmation.html');
 	}
 	
-	//give custom factory to all form values so they can change pages
-	$scope.fV = FormValues;
+	
 	
 	//Helps hide select options on movietime.html
 	$scope.ShowTime = false;
@@ -196,7 +109,7 @@ app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
 	
 	$scope.updateConcCost = function(concObj) {
 		//Function that gets cost of all the concessions
-		//and creates a string of all the items purcahsed to be put on the receipt.
+		//and creates a string of all the items purchased to be put on the receipt.
 		
 		//Get current values of all the objects selected
 		$scope.fV.concObjects = concObj;
@@ -221,9 +134,14 @@ app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
 				$scope.fV.concPurchases += itemsPurch; 
 			}
 		}
+	//Enable concItemsChosen to indicate a concessions item has been picked
+	$scope.fV.concItemsChosen = true
+	//Remove the final comma in string of purchases
+	$scope.fV.concPurchases = $scope.fV.concPurchases.substring(0, $scope.fV.concPurchases.length - 1);
 	}
 	
 	$scope.moviesChange = function() {
+		
 		//Save current movie times from select movie to be put into second select
 
 		//Reset the value for Movie Time Value model
@@ -231,7 +149,7 @@ app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
 		
 		$scope.movTimes = $scope.fV.movieChosen.times;
 		$scope.fV.movieCost = 0; 
-		$scope.moviesTicketAmt = 1;
+		$scope.fV.movieTickets = 1;
 			
 		//Show Rest of Items in Form
 		$scope.ShowTime = true;
@@ -250,19 +168,35 @@ app.controller('MoviezCtrl', function($http, $scope, $location, FormValues) {
 		$scope.updateMoviesCost($scope.fV.movieChosen.price);
 	}
 	
-	//update price of movie ticket based on movie selection, cost, and amount of tickets selected
+	//Update price of movie ticket based on movie selection, cost, and amount of tickets selected
 	$scope.updateMoviesCost = function(price) {
 		$scope.fV.movieCost = 0;
-		$scope.fV.movieCost = $scope.moviesTicketAmt * price;
+		$scope.fV.movieCost = $scope.fV.movieTickets * price;
+	}
+
+	
+	//Check if first two form pages are filled out properly
+	//If they are fillled properly display receipt
+	//Else display error message and prompt user to create a new order
+	$scope.finalPageCheck = function() {
+		console.log("into check");
+		if($scope.fV.form1 && $scope.fV.form2){
+			console.log("forms pass");
+			$scope.fV.finalForm =  true;
+			$scope.calcTotal();
+		} else {
+			console.log("forms fail");
+			$scope.fV.finalForm =  false;
+		}
 	}
 	
 	//Calc total on final page
 	$scope.calcTotal = function (){
+		console.log("run total");
 		$scope.fV.subTotal = $scope.fV.movieCost + $scope.fV.concTotalCost;
 		$scope.fV.tax = $scope.fV.subTotal * 0.15;
 		$scope.fV.finalCost = $scope.fV.tax + $scope.fV.subTotal;
 	}
-	
 	
 
 });
